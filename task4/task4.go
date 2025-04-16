1b618c7674707d4120400e9a99bbea36b389f2a9
Nikita <kanunov_n@rtlq.ru>
2025-04-16 03:48:31 +0200

    Добавлена задача 4

--- task4/task4.go
+++ task4/task4.go
@@ -1,4 +1,4 @@
-package task4
+package main
 
 import (
 	"context"
@@ -18,13 +18,30 @@ func get(ctx context.Context, id int) (string, error) {
 
 // Задание 4 вызывать не больше 4х запросов одновременно и не больше 1000 в минуту (leacky bucket rate-limit)
 func callAll(listToCall []int) map[int]string {
+
+	const rate = 4
+	const period = time.Minute / 1000
 	results := map[int]string{}
 	var mutex sync.Mutex
 	var wg sync.WaitGroup
+
+	semaphore := make(chan struct{}, rate)
+	limiter := time.NewTicker(period)
+	defer limiter.Stop()
+
 	for _, id := range listToCall {
 		wg.Add(1)
 		go func(id int) {
 			defer wg.Done()
+
+			<-limiter.C
+
+			semaphore <- struct{}{}
+
+			defer func() {
+				<-semaphore
+			}()
+
 			s, err := get(context.Background(), id)
 			if err != nil {
 				fmt.Println("error", id, err)
@@ -41,7 +58,7 @@ func callAll(listToCall []int) map[int]string {
 }
 
 func main() {
-	listToCall := []int{1, 2, 3, 4, 5}
+	listToCall := []int{1, 2, 3, 4, 5, 6, 7, 8, 9, 10}
 
 	results := callAll(listToCall)
 
